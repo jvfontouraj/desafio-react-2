9f85cb6a2bb365d60aa1f1d7ea7e845d432351c8
João Vitor F Jardim <61571174+jvfontouraj@users.noreply.github.com>
2023-03-30 23:08:30 -0300

    update

--- src/Pages/Success/Success.style.ts
+++ src/Pages/Success/Success.style.ts
@@ -2,8 +2,81 @@ import styled from 'styled-components'
 
 export const SuccessContainer = styled.section`
   display: flex;
+  flex-direction: column;
   width: 90rem;
   background: ${(props) => props.theme['background-']};
   height: calc(100vh - 6.5rem);
-  padding: 2.5rem 10rem;
+  padding: 5rem 10rem;
+
+  h2 {
+    font-family: 'Baloo 2';
+    font-weight: 800;
+    font-size: 2rem;
+    line-height: 130%;
+    color: ${(props) => props.theme['yellow-dark']};
+    margin-bottom: 0.25rem;
+  }
+
+  h3 {
+    font-weight: 400;
+    font-size: 1.25rem;
+    line-height: 130%;
+    color: ${(props) => props.theme['base-subtitle']};
+    font-stretch: 100;
+    margin-bottom: 2.5rem;
+  }
+
+  img {
+    display: flex;
+    align-self: center;
+    width: 30.75rem;
+    height: 18.31rem;
+  }
+
+  .alignCenterContainer {
+    display: flex;
+    gap: 6.375rem;
+  }
+`
+
+export const DeliveryInfoContainer = styled.div`
+  padding: 2.5rem;
+  padding-bottom: 0;
+  border: 1px solid ${(props) => props.theme['yellow-dark']};
+  border-radius: 6px 36px;
+  width: 32.875rem;
+`
+
+export const InfosContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 2rem;
+
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .mapIcon,
+  .timerIcon,
+  .dollarIcon {
+    box-sizing: content-box;
+    padding: 0.5rem;
+    color: white;
+    border-radius: 99999px;
+    margin-right: 0.75rem;
+  }
+
+  .mapIcon {
+    background-color: ${(props) => props.theme['purple-']};
+  }
+
+  .timerIcon {
+    background-color: ${(props) => props.theme['yellow-']};
+  }
+
+  .dollarIcon {
+    background-color: ${(props) => props.theme['yellow-dark']};
+  }
 `
--- src/Pages/Success/Success.tsx
+++ src/Pages/Success/Success.tsx
@@ -1,5 +1,10 @@
-import { SuccessContainer } from './Success.style'
+import {
+  DeliveryInfoContainer,
+  InfosContainer,
+  SuccessContainer,
+} from './Success.style'
 import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
+import successImage from '../../assets/Success-image.png'
 
 export function Success() {
   return (
@@ -7,33 +12,42 @@ export function Success() {
       <div>
         <h2>Uhu! Pedido confirmado</h2>
         <h3>Agora é só aguardar que logo o café chegará até você</h3>
-        <div>
-          <div>
-            <MapPin />
+      </div>
+      <div className="alignCenterContainer">
+        <DeliveryInfoContainer>
+          <InfosContainer>
+            <MapPin weight="fill" size={16} className="mapIcon" />
             <div>
               <span>
                 Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
               </span>
               <span>Farrapos - Porto Alegre, RS</span>
             </div>
-          </div>
-          <div>
-            <Timer />
-            <span>Previsão de entrega</span>
-            <span>
-              <strong>20 min - 30 min</strong>
-            </span>
-          </div>
-          <div>
-            <CurrencyDollar />
-            <span>Pagamento na entrega</span>
-            <span>
-              <strong>Cartão de Crédito</strong>
-            </span>
-          </div>
-        </div>
+          </InfosContainer>
+
+          <InfosContainer>
+            <Timer weight="fill" size={16} className="timerIcon" />
+            <div>
+              <span>Previsão de entrega</span>
+              <span>
+                <strong>20 min - 30 min</strong>
+              </span>
+            </div>
+          </InfosContainer>
+
+          <InfosContainer>
+            <CurrencyDollar weight="fill" size={16} className="dollarIcon" />
+            <div>
+              <span>Pagamento na entrega</span>
+              <span>
+                <strong>Cartão de Crédito</strong>
+              </span>
+            </div>
+          </InfosContainer>
+        </DeliveryInfoContainer>
+
+        <img src={successImage} alt="" />
       </div>
-      <img src="" alt="" />
     </SuccessContainer>
   )
 }
